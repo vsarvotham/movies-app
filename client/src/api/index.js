8b6de9750a5d2d41a7384e0259f76752fe797f1c
Sarvothaman Vittal <sarvothaman.vittal@gmail.com>
2021-06-16 00:53:28 +0530

    Change for docker compose

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'http://18.212.40.138:3000/api',
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
